4e23cebf145675e905e0b69e3840d7b4a3b3d804
Aran Hakki <aran.hakki@gmail.com>
2022-06-04 02:07:42 +0100

    removed comments

--- src/test/java/com/cognitionbox/petra/examples/sensorlightingsystem/LightingSystemMain.java
+++ src/test/java/com/cognitionbox/petra/examples/sensorlightingsystem/LightingSystemMain.java
@@ -5,11 +5,5 @@ import static com.cognitionbox.petra.lang.Petra.infiniteStart;
 public class LightingSystemMain {
     public static void main(String... args){
         infiniteStart(LightSystem::updateLight, LightSystem.class,1000);
-
-        // we to check the time interval, and make sure the period of the check aligns with the interval,
-        // so we dont miss any states.
-        // like trigger if time is 00:00, and ensure checks are performed every minute.
-        // only trigger when changes from day to night
-        //infiniteStart(LightSystem::updateLight, LightSystem.class, 1000);
     }
 }
